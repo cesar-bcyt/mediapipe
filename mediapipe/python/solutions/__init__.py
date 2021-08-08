07af08aa22985707af1c6de008bfc89765d5f38c
César Bravo <61810914+cesar-bcyt@users.noreply.github.com>
2021-08-08 15:31:05 -0400

    Update __init__.py

--- mediapipe/python/solutions/__init__.py
+++ mediapipe/python/solutions/__init__.py
@@ -14,10 +14,10 @@
 
 """MediaPipe Solutions Python API."""
 
-import mediapipe.python.solutions.drawing_styles
 import mediapipe.python.solutions.drawing_utils
 import mediapipe.python.solutions.face_detection
 import mediapipe.python.solutions.face_mesh
+import mediapipe.python.solutions.iris
 import mediapipe.python.solutions.hands
 import mediapipe.python.solutions.holistic
 import mediapipe.python.solutions.objectron
